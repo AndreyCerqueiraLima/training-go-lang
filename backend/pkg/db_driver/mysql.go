0e8df42002e1cc9db5c0cc01c8152f82f53f8d10
Andrey.Lima <andrey.lima@sovos.com>
2025-10-19 21:16:25 -0300

    criando dockerfile e docker-compose

--- backend/pkg/db_driver/mysql.go
+++ backend/pkg/db_driver/mysql.go
@@ -2,11 +2,20 @@ package db_driver
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 )
 
 func GetInstance() *sql.DB {
-	dsn := "root:@tcp(localhost:3306)/training_test_go"
+	db_user := os.Getenv("DB_USER")
+	//db_password := os.Getenv("DB_PASSWORD")
+	db_host := os.Getenv("DB_HOST")
+	db_database := os.Getenv("DB_DATABASE")
+	db_port := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("%s:@tcp(%s:%s)/%s", db_user, db_host, db_port, db_database)
+	fmt.Println(dsn)
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		log.Fatal(err)
